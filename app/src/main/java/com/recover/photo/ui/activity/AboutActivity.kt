4d878d9011528905407992b8fd0e279cf6edfe62
HTC <abasit@terafort.com>
2025-05-31 22:37:40 +0500

    media scanner crash resolved

--- app/src/main/java/com/recover/photo/ui/activity/AboutActivity.kt
+++ app/src/main/java/com/recover/photo/ui/activity/AboutActivity.kt
@@ -19,17 +19,6 @@ import androidx.core.app.NotificationManagerCompat
 import com.recover.photo.R
 import com.recover.photo.utils.AppUtils
 
-/**
- * A [PreferenceActivity] that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- *
- *
- * See [
- * Android Design: Settings](http://developer.android.com/design/patterns/settings.html) for design guidelines and the [Settings
- * API Guide](http://developer.android.com/guide/topics/ui/settings.html) for more information on developing a Settings UI.
- */
 class AboutActivity : AppCompatActivity() {
     private var isEnabled = false
     // private InterstitialAd mInterstitialAd;
